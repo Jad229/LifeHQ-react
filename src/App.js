0a14cb7345f0d5535f338dab9b8473c63085a193
Jad229 <jad229@njit.edu>
2023-06-09 13:02:21 -0400

    feat: task adding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Overview from "./components/Overview";
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div>
-      <Overview />
-      <input></input>
-      <button>Add Task</button>
+      <Overview task={taskList} />
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      ></input>
+      <button
+        onClick={() => {
+          setTaskList([...taskList, inputValue]);
+          setInputValue("");
+        }}
+      >
+        Add Task
+      </button>
     </div>
   );
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Overview extends Component {
-  render() {
-    return (
-      <div>
-        <ul></ul>
-      </div>
-    );
-  }
+export default function Overview(props) {
+  return (
+    <div>
+      <ul>
+        {props.task.map((task, idx) => {
+          return <li key={idx}>{task}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
